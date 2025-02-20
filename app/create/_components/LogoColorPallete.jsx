59e3dc375075aa16eb19fbe4fff26318bdefea73
Hafeezbaig <hafeezbaig3138@gmail.com>
2025-02-21 00:20:45 +0530

    new components added for the logo creation

---
+++ app/create/_components/LogoColorPallete.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LogoColorPallete() {
+  return (
+    <div>LogoColorPallete</div>
+  )
+}
+
+export default LogoColorPallete
--- app/create/page.jsx
+++ app/create/page.jsx
@@ -1,19 +1,36 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from 'react'
 import LogoTitle from './_components/LogoTitle'
 import { Button } from '@/components/ui/button'
 import { ArrowRight, ArrowLeft } from 'lucide-react'
 
-function page() {
+function CreateLogo() {
+    const [step, setStep] = useState(1);
+    const [formData,setFormData]=useState();
+
+    const onHandleInputChange=(field,value)=>{
+
+        setFormData(prev=>({
+            ...prev,
+            [field]:value
+        }))
+
+    }
   return (
     <div className='mt-28 p-10 border rounded-xl 2xl:mx-72 '>
-        <LogoTitle />
+        {step==1?
+        
+        <LogoTitle onHandleInputChange={(v)=>onHandleInputChange('title',v)} /> :
+        null
+  }
 
         <div className="flex items-center justify-between mt-10">
-            <Button variant="outline"> <ArrowLeft />Previous</Button>
-            <Button> <ArrowRight />Continue</Button>
+            {step!=1&& <Button variant="outline"> <ArrowLeft />Previous</Button>}
+            <Button onClick={()=>setStep(step+1)}> <ArrowRight />Continue</Button>
         </div>
     </div>
   )
 }
 
-export default page
+export default CreateLogo
