d6ad30bb008a0373a9f4860fede8676979cda5d3
Hafeezbaig <hafeezbaig3138@gmail.com>
2025-02-21 00:32:56 +0530

    logo description component updated

--- app/create/_components/LogoDesc.jsx
+++ app/create/_components/LogoDesc.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import HeadingDescription from './HeadingDescription'
 
-function LogoDesc() {
-  return (
-    <div>LogoDesc</div>
-  )
+function LogoDesc({onHandleInputChange}) {
+return (
+    <div className='my-10'>
+            <HeadingDescription 
+            title='Describe Your Logo Idea'
+            description='Share your ideas, themes, or any specific elements you want in your logo.' />
+
+<input type="text" placeholder="Enter your Logo Description" className='border border-gray-300 rounded-lg w-full p-4 mt-5' 
+        onChange={(e)=>onHandleInputChange(e.target.value)}
+        
+        />
+
+    </div>
+)
 }
 
 export default LogoDesc
--- app/create/page.jsx
+++ app/create/page.jsx
@@ -20,11 +20,12 @@ function CreateLogo() {
             [field]:value
         }))
 
+        console.log(formData)
+
     }
   return (
     <div className='mt-28 p-10 border rounded-xl 2xl:mx-72 '>
         {step==1?
-        
         <LogoTitle onHandleInputChange={(v)=>onHandleInputChange('title',v)} /> :
         step==2?
         <LogoDesc onHandleInputChange={(v)=>onHandleInputChange('desc',v)} />:
@@ -38,7 +39,7 @@ function CreateLogo() {
   }
 
         <div className="flex items-center justify-between mt-10">
-            {step!=1&& <Button variant="outline"> <ArrowLeft />Previous</Button>}
+            {step!=1&& <Button variant="outline" onClick={()=>setStep(step-1)}> <ArrowLeft />Previous</Button>}
             <Button onClick={()=>setStep(step+1)}> <ArrowRight />Continue</Button>
         </div>
     </div>
