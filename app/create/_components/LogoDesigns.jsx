2db160a62de184a2466b2c314ddc8f3d1f05b595
Hafeezbaig <hafeezbaig3138@gmail.com>
2025-02-22 17:41:49 +0530

    logo design component completed and bugs fixed

--- app/create/_components/LogoDesigns.jsx
+++ app/create/_components/LogoDesigns.jsx
@@ -4,17 +4,21 @@ import HeadingDescription from './HeadingDescription'
 import LogoDesig from '@/app/_data/LogoDesig'
 import Image from 'next/image'
 
-function LogoDesigns() {
+function LogoDesigns(onHandleInputChange) {
+  const [selectedOption, setSelectedOption] = useState();
   return (
     <div className='my-10'>
       <HeadingDescription 
       title='Choose Your Logo Style' 
       description='Select the type of logo design that best represents your brands unique identity.' />
        
-    <div>
+    <div className='grid grid-cols-2 md:grid-cols-3 gap-10 mt-10'>
     {LogoDesig.map((design,index)=>(
-      <div key={index}>
-        <Image src={design.image} alt={design.title} width={300} height={200} />
+      <div key={index} onClick={()=>{setSelectedOption(design.title);
+        onHandleInputChange(design)
+      }} 
+      className={`p-1 hover:border-2 border-primary rounded-xl cursor-pointer ${selectedOption==design.title&&'border-2 rounded-xl border-primary'}`}>
+        <Image src={design.image} alt={design.title} width={300} height={200} className='w-full rounded-xl h-[150px] object-cover' />
       </div>
   ))}
     </div>
