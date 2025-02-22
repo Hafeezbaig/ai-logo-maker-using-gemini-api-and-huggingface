9d2f1256a4667d231e4c9ec5dfd2714e516a60b8
Hafeezbaig <hafeezbaig3138@gmail.com>
2025-02-22 16:59:57 +0530

    logo design component updated and prompts added

--- app/create/_components/LogoDesigns.jsx
+++ app/create/_components/LogoDesigns.jsx
@@ -1,8 +1,25 @@
+import { Heading } from 'lucide-react'
 import React from 'react'
+import HeadingDescription from './HeadingDescription'
+import LogoDesig from '@/app/_data/LogoDesig'
+import Image from 'next/image'
 
 function LogoDesigns() {
   return (
-    <div>LogoDesigns</div>
+    <div className='my-10'>
+      <HeadingDescription 
+      title='Choose Your Logo Style' 
+      description='Select the type of logo design that best represents your brands unique identity.' />
+       
+    <div>
+    {LogoDesig.map((design,index)=>(
+      <div key={index}>
+        <Image src={design.image} alt={design.title} width={300} height={200} />
+      </div>
+  ))}
+    </div>
+    </div>
+
   )
 }
 
