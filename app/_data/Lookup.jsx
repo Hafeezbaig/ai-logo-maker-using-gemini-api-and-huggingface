d025b30809209b4cc9e70ad3b29262ca76c98781
Hafeezbaig <hafeezbaig3138@gmail.com>
2025-02-26 00:42:04 +0530

    Lookup file added

---
+++ app/_data/Lookup.jsx
@@ -0,0 +1,30 @@
+export default{
+    LoadingWaitTitle:'Your logo is being created',
+    pricingOption : [
+        {
+            title: 'Free',
+            icon: '/free.png', // Add your free plan icon here
+            features: [
+                '⚫ Generate unlimited logos for free',
+                '⚫ Longer wait times',
+                '⚫ Wait time: 30 seconds to 3 minutes',
+                '⚫ Limited Design Options and Quality',
+                '⚫ Slow (Not Recommended)'
+            ],
+            button:'Generate Free'
+        },
+        {
+            title: 'Premium',
+            icon: '/thunder.png', // Add your premium plan icon here
+            features: [
+                '⚫ Generate unlimited logos',
+                '⚫ Shorter wait times',
+                '⚫ Wait time: Less than 10 seconds',
+                '⚫ Only 5 Credits',
+                
+            ],
+            button:'Generate Free'
+        }
+    ],
+}
+
